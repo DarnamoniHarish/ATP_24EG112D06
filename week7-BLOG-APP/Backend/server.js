ee92b63378bea0fef6dcf5401f740de75f42e108
DarnamoniHarish <harishdarnamoni08@gmail.com>
2026-05-20 15:16:06 +0530

    backend deployement

--- week7-BLOG-APP/Backend/server.js
+++ week7-BLOG-APP/Backend/server.js
@@ -15,7 +15,7 @@ const app=exp()
 
 app.use(cors(
     {
-        origin:["http://localhost:5173",'capstoneproject-blush.vercel.app'],
+        origin:["http://localhost:5173",'https://capstoneproject-blush.vercel.app'],
         credentials:true,
 
     }
