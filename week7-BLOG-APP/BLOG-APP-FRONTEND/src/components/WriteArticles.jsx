a78d956473d745a84be58386af32a130a15de67b
DarnamoniHarish <harishdarnamoni08@gmail.com>
2026-05-21 09:14:23 +0530

    backend deployement

--- week7-BLOG-APP/BLOG-APP-FRONTEND/src/components/WriteArticles.jsx
+++ week7-BLOG-APP/BLOG-APP-FRONTEND/src/components/WriteArticles.jsx
@@ -38,11 +38,9 @@ function WriteArticles() {
       //set loading true
       setLoading(true);
       //make POST req to save new article
-      let res=await axios.post("https://atp-24eg112d06.onrender.com/author-api/article",articleObj,{
-    headers: { Authorization: `Bearer ${currentUser.token}` },
-  },{withCredentials:true})
+      let res=await axios.post("https://atp-24eg112d06.onrender.com/author-api/article",articleObj,{withCredentials:true})
       //navigate to AuthorArticle
-      if(res.status==201){
+      if(res.status==200){
         navigate("../articles");
        //navigate("./author-profile/articles")
       }
